f5181217213a3fdb1a7e54d62364aff41a44214d
Lautaro Vazquez <lauti1498@gmail.com>
2022-11-23 17:25:01 -0300

    products with PUG

--- app.js
+++ app.js
@@ -5,7 +5,43 @@ const server = app.listen(PORT, () => {
     console.log(`Server Up on port ${PORT}`)
 })
 
+app.use(express.json())
 app.set('views', './views')
 app.set('view engine', 'pug')
-app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    try {
+        res.render('form', {
+            titulo: 'Ingrese su producto',
+            firstLabel: 'Nombre del producto:',
+            secondLabel: 'Precio del producto:',
+            thirdLabel: 'URL del producto:'
+        })
+    } catch (error) {
+        res.send({status: 'Error', message: error.message})
+    }
+})
+
+let products = []
+
+app.post('/products', (req, res) => {
+    try {
+        let product = req.body
+        if (!product || !product.price || !product.url) {
+            res.send({status: 'Error', message: 'Faltan campos por completar'})
+        } else {
+            console.log('REQ.BODY', req.body)
+            let newId = products.length + 1
+            product.id = newId
+            products.push(product)
+            console.log({message: 'Saved product', product})
+            res.render('products', {
+                products: products
+            }) 
+        } 
+    } catch (error) {
+        res.send({status: 'Error', message: error.message})
+    }
+    
+})
